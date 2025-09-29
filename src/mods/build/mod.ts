e1a4d0a618611b0970c7b19957e2ad0088f16f59
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-09-29 15:44:13 +0700

    f

--- src/mods/build/mod.ts
+++ src/mods/build/mod.ts
@@ -3,7 +3,7 @@ import { Document, Window } from "happy-dom";
 import { readFileSync } from "node:fs";
 import path from "node:path";
 
-const entrypoints = ["./src/mods/app/index.html"]
+const entrypoints = ["./src/mods/app/index.html", "./src/mods/app/test/index.html"]
 
 const windows = new Map<string, Window>()
 const documents = new Map<string, Document>()
@@ -36,21 +36,87 @@ for (const entrypoint of entrypoints) {
   }
 }
 
-if (typeof Deno !== "undefined") {
-  const result = await Deno.bundle({
-    entrypoints: scripts,
-    format: "esm",
-    outputDir: "./dist"
-  })
+export async function* bundle(scripts: string[]) {
+  if (typeof Deno !== "undefined") {
+    const result = await Deno.bundle({
+      entrypoints: scripts,
+      format: "esm",
+      outputDir: "./dist",
+      codeSplitting: true,
+      minify: true,
+      write: false
+    })
 
-  console.log(result)
-} else {
-  const result = await esbuild.build({
-    entryPoints: scripts,
-    bundle: true,
-    format: "esm",
-    outdir: "./dist",
-  })
+    for (const warning of result.warnings)
+      console.warn(warning)
 
-  console.log(result)
+    for (const error of result.errors)
+      console.error(error)
+
+    if (result.errors.length)
+      throw new Error("Build failed")
+
+    if (result.outputFiles == null)
+      throw new Error("No output files")
+
+    for (const file of result.outputFiles)
+      yield { path: file.path, text: file.text(), hash: file.hash }
+
+    return
+  } else {
+    const result = await esbuild.build({
+      entryPoints: scripts,
+      bundle: true,
+      format: "esm",
+      outdir: "./dist",
+      splitting: true,
+      write: false
+    })
+
+    for (const warning of result.warnings)
+      console.warn(warning)
+
+    for (const error of result.errors)
+      console.error(error)
+
+    if (result.errors.length)
+      throw new Error("Build failed")
+
+    if (result.outputFiles == null)
+      throw new Error("No output files")
+
+    for (const file of result.outputFiles)
+      yield { path: file.path, text: file.text, hash: file.hash }
+
+    return
+  }
+}
+
+const outputs = new Array<{ path: string, text: string, hash: string }>()
+
+for await (const output of bundle(scripts)) {
+  console.log(output.path)
+
+  outputs.push(output)
+}
+
+for (const entrypoint of entrypoints) {
+  const window = windows.get(entrypoint)!
+  const document = documents.get(entrypoint)!
+
+  for (const element of document.querySelectorAll("script[type=bundle]")) {
+    const script = element as unknown as HTMLScriptElement
+
+    if (script.src) {
+      const url = new URL(script.src)
+
+      if (url.protocol === "file:") {
+        // const sibling = path.join(path.dirname(url.pathname), `${path.basename(url.pathname, ".ts")}.js`)
+      } else {
+        // TODO
+      }
+    } else {
+      // TODO
+    }
+  }
 }
